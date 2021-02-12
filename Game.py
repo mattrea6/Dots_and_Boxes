1b87c8ebf9d21c6ba80de5829f2e6197c50ec104
Matthew Rea <ream@cardiff.ac.uk>
2021-02-12 17:27:33 +0000

    Game rules now work!

--- Game.py
+++ Game.py
@@ -2,19 +2,31 @@ from Box import Box
 from Line import Line
 
 class Game:
-    def __init__(self, no_players, width, height):
+    def __init__(self, width, height, copy_grid=None, copy_boxes=None):
+        """
+        Initialise the game with given width and height.
+        If grid or boxes are passed, then create a copy of these objects.
+        Args:
+            width: int
+            height: int
+        """
         self.width = width
         self.height = height
-        self.no_players = no_players
-        self.build_game()
+        if copy_grid is None and copy_boxes is None:
+            self.build_game()
+        else:
+            self.build_copy(copy_grid, copy_boxes)
 
     def build_game(self):
-        # Build lists of horizontal and vertical lines
-        verts = [[Line() for j in range(self.height-1)] for i in range(self.width)]
-        horiz = [[Line() for j in range(self.width-1)] for i in range(self.height)]
-        # Then add to one list
-        self.grid = [horiz, verts]
-        # This means grid[0][0][0] is the top-left horizontal line
+        """
+        Builds an empty game board with internal width and height.
+        """
+        # Build two lists of horizontal and vertical lines.
+        self.grid = [
+            [[Line() for j in range(self.height-1)] for i in range(self.width)],
+            [[Line() for j in range(self.width-1)] for i in range(self.height)]
+        ]
+        # This means grid[0][0][0] is the top left horizontal line
         # grid[1][0][0] is the top left vertical line
         # Then build the box objects in a 2d list
         self.boxes = [[0 for i in range(self.width-1)] for j in range(self.height-1)]
@@ -23,30 +35,156 @@ class Game:
                 # Boxes are constructed with lines in the order [top, bottom, left, right]
                 self.boxes[i][j] = Box(self.grid[0][i][j], self.grid[0][i+1][j], self.grid[1][j][i], self.grid[1][j+1][i])
 
+    def build_copy(self, copy_grid, copy_boxes):
+        """
+        Builds a game board with copied values.
+        Args:
+            copy_grid: List[][][Line]
+            copy_boxes: List[][Box]
+        """
+        # Build two lists of horizontal and vertical lines using the input grid.
+        self.grid = [
+            [[Line(j.owner) for j in i] for i in copy_grid[0]],
+            [[Line(j.owner) for j in i] for i in copy_grid[1]]
+        ]
+        # This means grid[0][0][0] is the top left horizontal line
+        # grid[1][0][0] is the top left vertical line
+        # Then build the box objects in a 2d list
+        self.boxes = [[0 for i in range(self.width-1)] for j in range(self.height-1)]
+        for i in range(self.height-1):
+            for j in range(self.width-1):
+                # Boxes are constructed with lines in the order [top, bottom, left, right]
+                self.boxes[i][j] = Box(self.grid[0][i][j], self.grid[0][i+1][j], self.grid[1][j][i], self.grid[1][j+1][i], copy_boxes[i][j].owner)
+
+    def get_copy(self):
+        """
+        Game returns a deep copy of itself.
+        Returns:
+            Game
+        """
+        return Game(self.width, self.height, self.grid, self.boxes)
+
     def take_turn(self, player, move):
-        self.grid[move[0], move[1], move[2]].draw(player)
+        """
+        Takes a turn for a given player. Claims a line.
+        Returns bool for success. True if move is completed successfully.
+        Args:
+            player: int
+            move: 3-tuple(int)
+        Returns:
+            bool
+        """
+        # Some validation so only real moves can be played.
+        if move[0] == 0:
+            if move[1] > self.height or move[2] >= self.width:
+                print("Invalid move")
+                return False
+        elif move[0] == 1:
+            if move[1] > self.width or move[2] >= self.height:
+                print("Invalid move")
+                return False
+        else:
+            print("Invalid move")
+            return False
+
+        # Attempt to claim the line.
+        if self.grid[move[0]][move[1]][move[2]].draw(player):
+            # Now check the boxes that share this edge.
+            if move[0] == 0:
+                # Edge cases
+                if move[1] == 0:
+                    self.boxes[move[1]][move[2]].check_completed(player)
+                elif move[1] == self.height-1:
+                    self.boxes[move[1]-1][move[2]].check_completed(player)
+                # Middle case
+                else:
+                    self.boxes[move[1]-1][move[2]].check_completed(player)
+                    self.boxes[move[1]][move[2]].check_completed(player)
+            else:
+                if move[1] == 0:
+                    self.boxes[move[2]][move[1]].check_completed(player)
+                elif move[1] == self.width-1:
+                    self.boxes[move[2]][move[1]-1].check_completed(player)
+                else:
+                    self.boxes[move[2]][move[1]-1].check_completed(player)
+                    self.boxes[move[2]][move[1]].check_completed(player)
+            return True
+        else:
+            print("Line has already been claimed")
+            return False
+
+    def is_finished(self):
+        """
+        Checks if the game is finished or if there are still moves to be made.
+        Returns:
+            bool
+        """
+        for i in range(self.height-1):
+            for j in range(self.width-1):
+                if not grid[i][j]:
+                    return False
+        return True
+
+
+    def check_scores(self):
+        """
+        Print the scores for all players that have any score.
+        Also print number of unclaimed boxes.
+        """
+        scores = {0:0}
+        for i in range(self.height-1):
+            for j in range(self.width-1):
+                owner = self.boxes[i][j].owner
+                if owner in scores:
+                    scores[owner] += 1
+                else:
+                    scores[owner] = 1
+        print("Unclaimed Boxes: {}".format(scores.pop(0)))
+        for key, value in scores.items():
+            print("Player {}: {}".format(key, value))
+
+    def check_score(self, player):
+        """
+        Get and return the score for one player.
+        Args:
+            player: int
+        Returns:
+            int
+        """
+        score = 0
+        for i in range(self.height-1):
+            for j in range(self.width-1):
+                if self.boxes[i][j].owner == player:
+                    score += 1
+        return score
 
     def print_grid(self):
+        """
+        Prints an ascii representation of the board.
+        For horizontal edges, '- -' is unclaimed, '---' is claimed
+        For vertical edges, '¦' is unclaimed, '|' is claimed
+        An empty box is unclaimed. A box with a number in is claimed.
+        """
         for i in range(self.height):
             for j in range(self.width):
                 if j != self.width-1:
                     print("*", end="")
                     if self.grid[0][i][j]:
-                        print("- -", end="")
-                    else:
                         print("---", end="")
+                    else:
+                        print("- -", end="")
                 else:
                     print("*")
             if i != self.height-1:
                 for j in range(self.width):
                     if j != self.width-1:
                         if self.grid[1][j][i]:
-                            print("¦", end="")
-                        else:
                             print("|", end="")
+                        else:
+                            print("¦", end="")
                         print(" {} ".format(self.boxes[i][j]), end="")
                     else:
                         if self.grid[1][j][i]:
-                            print("¦")
-                        else:
                             print("|")
+                        else:
+                            print("¦")
--- Line.py
+++ Line.py
@@ -1,9 +1,27 @@
 class Line:
-    def __init__(self):
-        self.owner = 0
+    def __init__(self, owner=0):
+        """
+        Initialise new Line object.
+        Args:
+            owner: int
+        """
+        self.owner = owner
 
     def draw(self, player):
-        self.owner = player
+        """
+        'draw' a line for a certain player. That player then owns the line.
+        Args:
+            player: int
+        Returns:
+            bool: True if line is successfully claimed.
+        """
+        if self.owner == 0:
+            self.owner = player
+            return True
+        return False
 
     def __bool__(self):
-        return self.owner == 0
+        """
+        Define truth value for line. If the line is owned, Line is True.
+        """
+        return self.owner != 0
