53f85c561281454b134ebdcdf5e55068dbea6934
Matthew Rea <ream@cardiff.ac.uk>
2021-05-24 16:02:17 +0100

    GUI Updates.

--- DotsAndBoxes/GameGUI.py
+++ DotsAndBoxes/GameGUI.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
     QPushButton, QApplication, QLabel, QSpinBox,
-    QComboBox, QColorDialog, QDoubleSpinBox, QRadioButton)
+    QComboBox, QColorDialog, QDoubleSpinBox, QRadioButton,
+    QMessageBox)
 from PyQt5.QtGui import QFont
 from PyQt5 import QtCore
 from Game import Game
@@ -25,7 +26,7 @@ class StartFrame(QWidget):
         Has a button that launches the game window.
         """
         self.setWindowTitle("Dots and Boxes")
-        self.setGeometry(700, 200, 600, 450)
+        self.setGeometry(50, 50, 600, 450)
         # Title label
         titleLabel = QLabel("Dots & Boxes", self)
         titleLabel.setFont(QFont('Arial', 20))
@@ -35,6 +36,11 @@ class StartFrame(QWidget):
         instLabel = QLabel("Choose board size and players, then click 'Start Game'!", self)
         instLabel.resize(instLabel.sizeHint())
         instLabel.move(175, 65)
+        # Make a button for the instructions pane.
+        instructionsButton = QPushButton('Instructions', self)
+        instructionsButton.resize(instructionsButton.sizeHint())
+        instructionsButton.move(450, 62)
+        instructionsButton.clicked.connect(self.instructions)
         # Input for size
         # Two number input boxes
         widthLabel = QLabel("Width:", self)
@@ -44,6 +50,7 @@ class StartFrame(QWidget):
         self.widthInput.resize(self.widthInput.sizeHint())
         self.widthInput.move(220, 100)
         self.widthInput.setRange(3, 10)
+        self.widthInput.setToolTip("Select the width of the game board. This is the number of dots in each row.")
 
         heightLabel = QLabel("Height:", self)
         heightLabel.resize(heightLabel.sizeHint())
@@ -52,98 +59,112 @@ class StartFrame(QWidget):
         self.heightInput.resize(self.heightInput.sizeHint())
         self.heightInput.move(350, 100)
         self.heightInput.setRange(3, 10)
+        self.heightInput.setToolTip("Select the height of the game board. This is the number of dots in each column.")
 
         # Input for players. Dropdowns filled with values from factory
         # Create label first
         playerOneLabel = QLabel("Player One:", self)
         playerOneLabel.resize(playerOneLabel.sizeHint())
-        playerOneLabel.move(170, 183)
+        playerOneLabel.move(170, 153)
         # Then create the dropdown and size it
         self.playerOneDropdown = QComboBox(self)
         self.playerOneDropdown.resize(100, 20)
-        self.playerOneDropdown.move(240, 180)
+        self.playerOneDropdown.move(240, 150)
         self.playerOneDropdown.currentIndexChanged.connect(self.playerPickerChanged)
+        self.playerOneDropdown.setToolTip("Choose the player type for Player 1.")
         # Create the colour picker button for player colour
         self.playerOneColour = QPushButton("Colour", self)
         self.playerOneColour.resize(self.playerOneColour.sizeHint())
-        self.playerOneColour.move(350, 180)
+        self.playerOneColour.move(350, 150)
         self.playerOneColour.setStyleSheet("background-color: {}".format(self.p1Col))
         self.playerOneColour.clicked.connect(self.playerOneColourPicker)
+        self.playerOneColour.setToolTip("Select the colour for Player 1s lines and boxes.")
 
         # These are the optional boxes that should only appear for specific player types.
         self.playerOneTimeLabel = QLabel("P1 Time Limit:", self)
         self.playerOneTimeLabel.resize(0,0)
-        self.playerOneTimeLabel.move(100, 208)
+        self.playerOneTimeLabel.move(170, 178)
 
         self.playerOneTimeLimit = QDoubleSpinBox(self)
         self.playerOneTimeLimit.resize(0,0)
-        self.playerOneTimeLimit.move(170, 205)
+        self.playerOneTimeLimit.move(240, 175)
         self.playerOneTimeLimit.setRange(0.2, 20.0)
         self.playerOneTimeLimit.setSingleStep(0.1)
+        self.playerOneTimeLimit.setToolTip("Select the amount of time available to Player 1 to take their turn.")
 
         self.playerOneMaxDepthLabel = QLabel("P1 Max Depth:", self)
         self.playerOneMaxDepthLabel.resize(0,0)
-        self.playerOneMaxDepthLabel.move(240, 208)
+        self.playerOneMaxDepthLabel.move(310, 178)
 
         self.playerOneMaxDepth = QSpinBox(self)
         self.playerOneMaxDepth.resize(0,0)
-        self.playerOneMaxDepth.move(315, 205)
+        self.playerOneMaxDepth.move(385, 175)
         self.playerOneMaxDepth.setRange(2, 50)
         self.playerOneMaxDepth.setValue(15)
+        self.playerOneMaxDepth.setToolTip("Select the max search depth that Minimax can reach.")
 
         self.playerOneCValueLabel = QLabel("P1 C Value:", self)
         self.playerOneCValueLabel.resize(0,0)
-        self.playerOneCValueLabel.move(240, 208)
+        self.playerOneCValueLabel.move(310, 178)
 
         self.playerOneCValue = QDoubleSpinBox(self)
         self.playerOneCValue.resize(0,0)
-        self.playerOneCValue.move(300, 205)
+        self.playerOneCValue.move(370, 175)
         self.playerOneCValue.setRange(0.0, 10.0)
         self.playerOneCValue.setSingleStep(0.05)
+        self.playerOneCValue.setValue(1.4)
+        self.playerOneCValue.setToolTip("Select the exploration coefficient for Monte Carlo Tree Search.")
 
 
         playerTwoLabel = QLabel("Player Two:", self)
         playerTwoLabel.resize(playerTwoLabel.sizeHint())
-        playerTwoLabel.move(100, 243)
+        playerTwoLabel.move(170, 233)
         self.playerTwoDropdown = QComboBox(self)
         self.playerTwoDropdown.resize(100, 20)
-        self.playerTwoDropdown.move(170, 240)
+        self.playerTwoDropdown.move(240, 230)
         self.playerTwoDropdown.currentIndexChanged.connect(self.playerPickerChanged)
+        self.playerTwoDropdown.setToolTip("Choose the player type for Player 2.")
+
         self.playerTwoColour = QPushButton("Colour", self)
         self.playerTwoColour.resize(self.playerTwoColour.sizeHint())
-        self.playerTwoColour.move(280, 240)
+        self.playerTwoColour.move(350, 230)
         self.playerTwoColour.setStyleSheet("background-color: {}".format(self.p2Col))
         self.playerTwoColour.clicked.connect(self.playerTwoColourPicker)
+        self.playerTwoColour.setToolTip("Select the colour for Player 2s lines and boxes.")
 
         self.playerTwoTimeLabel = QLabel("P2 Time Limit:", self)
         self.playerTwoTimeLabel.resize(0,0)
-        self.playerTwoTimeLabel.move(100, 268)
+        self.playerTwoTimeLabel.move(170, 258)
 
         self.playerTwoTimeLimit = QDoubleSpinBox(self)
         self.playerTwoTimeLimit.resize(0,0)
-        self.playerTwoTimeLimit.move(170, 265)
+        self.playerTwoTimeLimit.move(240, 255)
         self.playerTwoTimeLimit.setRange(0.2, 20.0)
         self.playerTwoTimeLimit.setSingleStep(0.1)
+        self.playerTwoTimeLimit.setToolTip("Select the amount of time available to Player 2 to take their turn.")
 
         self.playerTwoMaxDepthLabel = QLabel("P2 Max Depth:", self)
         self.playerTwoMaxDepthLabel.resize(0,0)
-        self.playerTwoMaxDepthLabel.move(240, 268)
+        self.playerTwoMaxDepthLabel.move(310, 258)
 
         self.playerTwoMaxDepth = QSpinBox(self)
         self.playerTwoMaxDepth.resize(0,0)
-        self.playerTwoMaxDepth.move(315, 265)
+        self.playerTwoMaxDepth.move(385, 255)
         self.playerTwoMaxDepth.setRange(2, 50)
         self.playerTwoMaxDepth.setValue(15)
+        self.playerTwoMaxDepth.setToolTip("Select the max search depth that Minimax can reach.")
 
         self.playerTwoCValueLabel = QLabel("P2 C Value:", self)
         self.playerTwoCValueLabel.resize(0,0)
-        self.playerTwoCValueLabel.move(240, 268)
+        self.playerTwoCValueLabel.move(310, 258)
 
         self.playerTwoCValue = QDoubleSpinBox(self)
         self.playerTwoCValue.resize(0,0)
-        self.playerTwoCValue.move(300, 265)
+        self.playerTwoCValue.move(370, 255)
         self.playerTwoCValue.setRange(0.0, 10.0)
         self.playerTwoCValue.setSingleStep(0.05)
+        self.playerTwoCValue.setValue(1.4)
+        self.playerTwoCValue.setToolTip("Select the exploration coefficient for Monte Carlo Tree Search.")
 
         # Populate both dropdowns with the player types in Player Factory.
         for player in self.playerFactory.playerTypes:
@@ -152,23 +173,47 @@ class StartFrame(QWidget):
 
         variantLabel = QLabel("Select Game Variant:", self)
         variantLabel.resize(variantLabel.sizeHint())
-        variantLabel.move(100, 300)
+        variantLabel.move(250, 300)
 
         self.normalGameRadio = QRadioButton("American Game", self)
-        self.normalGameRadio.move(100, 320)
+        self.normalGameRadio.move(160, 320)
         self.normalGameRadio.setChecked(True)
+        self.normalGameRadio.setToolTip("Play the standard board type, with no lines filled in.")
         self.swedishGameRadio = QRadioButton("Swedish Game", self)
-        self.swedishGameRadio.move(200, 320)
+        self.swedishGameRadio.move(260, 320)
+        self.swedishGameRadio.setToolTip("Play the 'Swedish' game variant, with the outer lines filled in from the start.")
         self.randomGameRadio = QRadioButton("Random Game", self)
-        self.randomGameRadio.move(295, 320)
+        self.randomGameRadio.move(355, 320)
+        self.randomGameRadio.setToolTip("Play a 'Random' game variant, with a random selection of lines filled in from the start.")
 
         # Button to start game
         startButton = QPushButton('Start Game', self)
-        startButton.resize(startButton.sizeHint())
-        startButton.move(100, 350)
+        startButton.resize(100, 40)
+        startButton.move(250, 350)
         startButton.clicked.connect(self.startGame)
 
         self.show()
+
+    def instructions(self):
+        """
+        Callback for instructions button. Creates a popup with instruction text.
+        """
+        message = QMessageBox()
+        message.setWindowTitle("Instructions")
+        message.setText("Dots and Boxes is a 'pencil and paper' game for two players.")
+        message.setInformativeText("""The game is played on a grid of dots that are connected by lines.\n
+        Four lines in a square pattern enclose a box.\n
+        The aim of the game is to capture more boxes than your opponent!\n
+        Each turn, a player can fill in a line by clicking that line.\n
+        If a player fills in a line and completes the fourth side of a box, they capture that box!\n
+        The player who captured the box also gets another turn.\n\n
+There are three variants of the game:\n
+        The 'American' variant - This is played on a blank grid.\n
+        The 'Swedish' varaint - This is played with the outer edges of the grid filled in.\n
+        The 'Random' variant - This is played with a random selection of lines filled in.
+        """)
+        message.setIcon(QMessageBox.Information)
+        x = message.exec_()
 
     def playerPickerChanged(self):
         """
@@ -313,30 +358,35 @@ class GameFrame(QWidget):
         Set up the grid of lines as buttons that can be clicked, and set the boxes
         as labels with no value.
         """
+        # work out window size before making
+        winWidth = (self.boxSize + self.lineWidth) * (self.width-1) + self.lineWidth + 20
+        winHeight = 100 +  (self.boxSize + self.lineWidth) * (self.height-1) + self.lineWidth + 20
         # Set title
         self.setWindowTitle("Dots and Boxes")
-        self.setGeometry(700, 200, 600, 450)
+        self.setGeometry(50, 50, winWidth, winHeight)
 
         # Set turn label
-        self.titleLabel = QLabel("Player 1's turn!", self)
+        self.titleLabel = QLabel("Player 1", self)
+        self.titleLabel.setFont(QFont('Arial', 16))
         self.titleLabel.resize(self.titleLabel.sizeHint())
-        self.titleLabel.move(100, 50)
+        self.titleLabel.move((winWidth/2)-40, 30)
 
         # Create winner label with no text
         self.winnerLabel = QLabel("", self)
-        self.winnerLabel.resize(self.winnerLabel.sizeHint())
+        self.winnerLabel.setFont(QFont('Arial', 16))
+        self.winnerLabel.move((winWidth/2) - 65, 20)
+        self.winnerLabel.resize(0,0)
 
         # Create Play Again button but do not make visible.
         self.replayButton = QPushButton("Play Again!", self)
-        self.replayButton.resize(self.replayButton.sizeHint())
-        self.replayButton.move(1000, 1000)
+        self.replayButton.resize(0,0)
+        self.replayButton.move((winWidth/2)-50, 50)
         self.replayButton.clicked.connect(self.replay)
 
+
         # Build board.
-        # Lists of buttons?
-        # Or just objects that can be clicked?
-        #who nose
-        topLeftX = 100
+        # Lists of buttons
+        topLeftX = 10
         topLeftY = 100
         # Build an array of button objects - this is the same list constructor as
         # the one to build the array of Lines in Game.
@@ -351,6 +401,11 @@ class GameFrame(QWidget):
         for i in range(self.height):
             x = topLeftX+self.lineWidth
             for j in range(self.width-1):
+                # create a 'dot'
+                l = QLabel('',self)
+                l.move(x-self.lineWidth, y)
+                l.resize(self.lineWidth, self.lineWidth)
+                l.setStyleSheet("border: 5px solid black; border-radius: 5px")
                 # Make it the right size & orientation
                 self.buttonGrid[o][i][j].resize(self.boxSize, self.lineWidth)
                 # Place it
@@ -364,6 +419,11 @@ class GameFrame(QWidget):
                 self.buttonGrid[o][i][j].setEnabled(False)
                 # Adjust the offsets for next button
                 x = x + self.lineWidth + self.boxSize
+            # create an end dot
+            l = QLabel('',self)
+            l.move(x-self.lineWidth, y)
+            l.resize(self.lineWidth, self.lineWidth)
+            l.setStyleSheet("border: 5px solid black; border-radius: 5px")
             y = y + self.lineWidth + self.boxSize
 
         # now do it again for the verticals
@@ -428,7 +488,7 @@ class GameFrame(QWidget):
         Updates the display after each player's turn.
         """
         # Update turn label
-        self.titleLabel.setText("Player {}s turn!".format(self.game.currentPlayer))
+        self.titleLabel.setText("Player {}".format(self.game.currentPlayer))
         self.titleLabel.resize(self.titleLabel.sizeHint())
         # update line colours
         for i in range(self.height):
@@ -475,8 +535,9 @@ class GameFrame(QWidget):
             print(winnerStr)
             self.winnerLabel.setText(winnerStr)
             self.winnerLabel.resize(self.winnerLabel.sizeHint())
-            self.winnerLabel.move(200, 50)
-            self.replayButton.move(250, 75)
+            #self.winnerLabel.move(200, 50)
+            self.replayButton.resize(100, 40)
+            #self.replayButton.resize(self.replayButton.sizeHint())
             # If a results filename has been passed to GameFrame then save the
             # game stats to this location and close the frame.
             if self.resultsFilename:
